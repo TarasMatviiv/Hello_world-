04fa1019e31028f2e89f0ef1ed80ede89a99e2b6
TarasMatviiv <taras.matviiv.ihorovych@gmail.com>
2017-09-01 11:03:27 +0300

    Added a new comment

--- Hello world/src/world/World.java
+++ Hello world/src/world/World.java
@@ -2,6 +2,6 @@ package world;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!"); //print "Hello world"
     }
 }
